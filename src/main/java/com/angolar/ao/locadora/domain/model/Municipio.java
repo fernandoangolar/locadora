77c0b1ea2dbc9f96925acc16a83d72dd2132b0bd
Fernando Angolar <fernandoangolar581@gmail.com>
2023-09-13 20:13:56 +0100

    Alterando o packege do model, foi adicionado a classe municipio

---
+++ src/main/java/com/angolar/ao/locadora/domain/model/Municipio.java
@@ -0,0 +1,20 @@
+package com.angolar.ao.locadora.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Municipio {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
