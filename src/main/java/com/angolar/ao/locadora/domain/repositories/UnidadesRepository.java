fbcd0ac8c5251d3e0ba7a19f694d5bd9c7ea0d72
Fernando Angolar <fernandoangolar581@gmail.com>
2023-11-14 21:14:09 +0100

    Refatorando o projeto

--- src/main/java/com/angolar/ao/locadora/domain/repositories/UnidadesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.angolar.ao.locadora.domain.repositories;
-
-import com.angolar.ao.locadora.domain.model.Unidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UnidadesRepository extends JpaRepository<Unidade, Long> {
-}
